8f188f7b7ad38c303d7837687bce8fe59b10349b
EC2 Default User <ec2-user@ip-172-31-21-110.ap-northeast-1.compute.internal>
2022-09-16 16:53:49 +0000

    [Finish]アプリケーションを完成させよう2-14

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,18 @@ class User < ApplicationRecord
 
 has_many :post_images, dependent: :destroy
 #1:N の「1」側にあたるモデルに、has_many を記載する必要がある
+
+has_one_attached :profile_image
+#ActiveStorageで、
+#profile_imageという名前でプロフィール画像を保存できるよう設定した。
+
+  def get_profile_image
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+   profile_image.variant(resize_to_limit: [width, height]).processed
+    #画像サイズの変換
+  end
+
 end
