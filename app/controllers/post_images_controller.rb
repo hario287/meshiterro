d32a8d47d4b709995b27e78f938575318e623d20
EC2 Default User <ec2-user@ip-172-31-21-110.ap-northeast-1.compute.internal>
2022-09-16 15:57:28 +0000

    [Finish]アプリケーションを完成させよう2-10

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,10 +1,39 @@
 class PostImagesController < ApplicationController
   def new
+    @post_image = PostImage.new
+    #@post_imagesは、form_with に渡すため用意された「空のモデル」
   end
+
+   # 投稿データの保存
+  def create
+    @post_image = PostImage.new(post_image_params)
+    @post_image.user_id = current_user.id
+    @post_image.save
+    redirect_to post_images_path
+  end
+    ##空のモデル(@post_images)では、PostImageモデルで定義したカラムを
+    #"[モデル名].[カラム名]"という形で繋げることで、
+    #保存するカラムの中身を操作することができます
+    ##current_user は、コードに記述するだけで、
+    #ログイン中のユーザーの情報を取得できる便利な記述です。
+    #ヘルパーメソッドと呼ばれるものの一種で、
+    #devise をインストールすることで使えるようになります。
+      ##ログイン中のユーザーのidを取得することができるようになってる
+
 
   def index
   end
 
   def show
+  end
+
+  def destroy
+  end
+
+   # 投稿データのストロングパラメータ
+  private
+
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caption)
   end
 end
